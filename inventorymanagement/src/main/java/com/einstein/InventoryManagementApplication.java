1ffc85ae307e7aa4c3e2939aa031e678284a012b
shriram061 <75435984+shriram061@users.noreply.github.com>
2023-05-08 17:40:48 +0530

    Redis commit

--- inventorymanagement/src/main/java/com/einstein/InventoryManagementApplication.java
+++ inventorymanagement/src/main/java/com/einstein/InventoryManagementApplication.java
@@ -2,6 +2,7 @@ package com.einstein;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients
 @SpringBootApplication
 @EnableSwagger2
+@EnableCaching
 public class InventoryManagementApplication {
 
 	public static void main(String[] args) {
